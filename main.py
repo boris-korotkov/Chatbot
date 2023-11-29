290c370d1a360ba4cac21f42fc8d63945ad4b85f
Boris Korotkov <12427856+boris-korotkov@users.noreply.github.com>
2023-11-29 18:26:51 -0500

    The first working prototype

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRaisedButton
 import openai
+import config
+from openai import OpenAI
+
 
 # Set your OpenAI API key
-openai.api_key = ''
+openai.api_key = config.api_key
+
+#client = OpenAI(
+#    api_key=config.api_key,
+#)
+
 
 KV = '''
 BoxLayout:
@@ -39,14 +47,28 @@ class ChatApp(MDApp):
         user_input = self.root.ids.user_input.text
 
         # Make a call to the OpenAI GPT-3 API
+
+        response = openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                        messages=[
+                                    {
+                                                    "role": "user",
+                                                    "content": user_input,
+                                                    },
+                                                    ],
+)
+        """
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=user_input,
             max_tokens=100
         )
+        """
 
         # Extract and display the bot's response
-        bot_response = response.choices[0].text.strip()
+        #bot_response = response['choices'][0]['text'].strip()
+        bot_response = response.choices[0].message.content
+        #bot_response = response.choices[0].text.strip() - old syntax
         self.root.ids.response_label.text = f"Bot: {bot_response}"
 
         # Clear user input for the next interaction
